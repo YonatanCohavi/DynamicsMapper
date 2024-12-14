bae2e4a708814399fb7d9be9deb19615c84ec3a7
YonatanCohavi <yonatan.cohavi@gmail.com>
2024-12-14 19:04:04 +0200

    lazy mappers

--- src/DynamicsMapper.FastMappers/FastMapper.cs
+++ src/DynamicsMapper.FastMappers/FastMapper.cs
@@ -5,10 +5,10 @@ using System.Linq.Expressions;
 namespace DynamicsMapper.FastMappers;
 public class FastMapper<TModel, TPModel>(Expression<Func<TModel, TPModel>> selector)
 {
-    private readonly FastModelMapper<TPModel> fastModelMapper = MapperFactory.ToModelMapper(selector);
-    private readonly FastEntityMapper<TPModel> fastEntityMapper = MapperFactory.ToEntityMapper(selector);
-    public TPModel Map(Entity e) => fastModelMapper.Map(e);
-    public TPModel Map(Entity e, DynamicsMapperSettings settings) => fastModelMapper.Map(e, settings);
-    public Entity Map(TPModel model) => fastEntityMapper.Map(model);
-    public Entity Map(TPModel model, DynamicsMapperSettings settings) => fastEntityMapper.Map(model, settings);
+    private readonly Lazy<FastModelMapper<TPModel>> fastModelMapper = new Lazy<FastModelMapper<TPModel>>(() => MapperFactory.ToModelMapper(selector));
+    private readonly Lazy<FastEntityMapper<TPModel>> fastEntityMapper = new Lazy<FastEntityMapper<TPModel>>(() => MapperFactory.ToEntityMapper(selector));
+    public TPModel Map(Entity e) => fastModelMapper.Value.Map(e);
+    public TPModel Map(Entity e, DynamicsMapperSettings settings) => fastModelMapper.Value.Map(e, settings);
+    public Entity Map(TPModel model) => fastEntityMapper.Value.Map(model);
+    public Entity Map(TPModel model, DynamicsMapperSettings settings) => fastEntityMapper.Value.Map(model, settings);
 }
